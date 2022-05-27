d18f509fe4117c3386e28f23bd6506353bc916d3
Szergio <leirbag875420@gmail.com>
2022-05-27 14:58:13 -0300

    Testando script

--- modulo/modulo3/teste/assets/js/script.js
+++ modulo/modulo3/teste/assets/js/script.js
@@ -1,1 +1,10 @@
-console.log("teste");
+function soma(a,b){
+    var soma = 0;
+    soma = a + b;
+    return soma;
+}
+var a,b,result = 0;
+a = 10;
+b = 27;
+result = soma(a,b);
+console.log("O result =",result);
